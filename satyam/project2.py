679d48029c476d3e10d68d23038b9fd552397c31
kumar satyam <kumarsatayam51@gmail.com>
2025-08-31 00:54:15 +0530

    updating my project

--- satyam/project2.py
+++ satyam/project2.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -9,12 +10,25 @@ def translate(word):
     elif word.title() in data:
         return data[word.title()]
     elif word.upper() in data:
-        return data
-
+        return data[word.upper()]
+    elif len(get_close_matches(word , data.keys())) > 0 :
+        print("did you mean %s instead" %get_close_matches(word, data.keys())[0])
+        decide = input("press y for yes or n for no")
+        if decide == "y":
+            return data[get_close_matches(word , data.keys())[0]]
+        elif decide == "n":
+            return("pugger your paw steps on wrong keys ")
+        else:
+            return("You have entered wrong input please enter just y or n")
     else:
-        return "pugger your paw steps on wrong keys"
+        print("pugger your paw steps on wrong keys")
 
 
-word = input("Enter the word you want to search: ")
+
+word = input("Enter the word you want to search")
 output = translate(word)
-print(output)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
